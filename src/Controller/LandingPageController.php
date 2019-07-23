632ea39acf8ff7e80c53cb66e93d48f5c91a1998
InovaXion <mmghaoues@gmail.com>
2019-07-23 17:03:01 +0200

    update

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GuzzleHttp\Client as ClientsGuzzle;
 
 class LandingPageController extends Controller
 {
@@ -31,6 +32,48 @@ class LandingPageController extends Controller
             $entityManager->persist($order);
             $entityManager->flush();
 
+            $client = new ClientsGuzzle([
+                'base_uri' => 'https://api-commerce.simplon-roanne.com/',
+                'headers' => ['Authorization' => 'Bearer mJxTXVXMfRzLg6ZdhUhM4F6Eutcm1ZiPk4fNmvBMxyNR4ciRsc8v0hOmlzA0vTaX']
+                ]);
+
+            $response = $client->request('POST', '/order', [
+                'json'    => [
+                    "order"=> [
+                    "id"=> $order->getId(),
+                    "product"=>$order->getProduct(),
+                    "payment_method"=>$order->getPaymentMethod(),
+                    "status"=>$order->getStatus(),
+                    "client"=> [
+                      "firstname"=> $order->getClient()->getFirstname(),
+                      "lastname"=> $order->getClient()->getLastname(),
+                      "email"=> $order->getClient()->getEmail()
+                    ],
+                    "addresses"=> [
+                      "billing"=> [
+                        "address_line1"=> $order->getAddressesBilling()->getAddressLine1(),
+                        "address_line2"=>$order->getAddressesBilling()->getAddressLine2(),
+                        "city"=> $order->getAddressesBilling()->getCity(),
+                        "zipcode"=> $order->getAddressesBilling()->getZipCode(),
+                        "country"=> $order->getAddressesBilling()->getCountry(),
+                        "phone"=> $order->getAddressesBilling()->getPhone()
+                      ],
+                      "shipping"=> [
+                        "address_line1"=> $order->getAddressesShipping()->getAddressLine1(),
+                        "address_line2"=> $order->getAddressesShipping()->getAddressLine2(),
+                        "city"=> $order->getAddressesShipping()->getCity(),
+                        "zipcode"=> $order->getAddressesShipping()->getZipCode(),
+                        "country"=> $order->getAddressesShipping()->getCountry(),
+                        "phone"=> $order->getAddressesShipping()->getPhone()
+                      ]
+                    ]
+                  ]
+          ]
+      ]);
+      
+      dd($response->getBody()->getContents());
+
+
             return $this->redirectToRoute('payment');
         }
 
@@ -49,53 +92,37 @@ class LandingPageController extends Controller
     /**
      * @Route("/payment", name="payment")
      */
-    public function payment()
+    public function payment(Request $request)
     {
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'https://api.github.com/user', [
-            'auth' => ['user', 'pass']
-        ]);
-        echo $res->getStatusCode();
-        // "200"
-        echo $res->getHeader('content-type')[0];
-        // 'application/json; charset=utf8'
-        echo $res->getBody();
-        // {"type":"User"...'
-        
-        // Send an asynchronous request.
-        $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
-        $promise = $client->sendAsync($request)->then(function ($response) {
-            echo 'I completed! ' . $response->getBody();
-        });
-        $promise->wait();  
+         return $this->render('landing_page/confirmation.html.twig', []);
     }
 
 
 
 
-    /**
-     * @Route("/", name="OrderType_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
+    // /**
+    //  * @Route("/", name="OrderType_new", methods={"GET","POST"})
+    //  */
+    // public function new(Request $request): Response
+    // {
 
 
-        $order = new Orders;
+    //     $order = new Orders;
 
-        $form = $this->createForm(OrderType::class, $order);
-        $form->handleRequest($request);
+    //     $form = $this->createForm(OrderType::class, $order);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($order);
-            $entityManager->flush();
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($order);
+    //         $entityManager->flush();
 
-            return $this->redirectToRoute('payment');
-        }
+    //         return $this->redirectToRoute('payment');
+    //     }
 
-        return $this->render('landing_page/test.html.twig', [
+    //     return $this->render('landing_page/test.html.twig', [
 
-            'form' => $form->createView(),
-        ]);
-    }
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
 }
